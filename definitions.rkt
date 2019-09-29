3c519c9d391d388df7143ec2a96cf4bf2c5c604f
asa <asa@localhost.localdomain>
2019-09-29 18:10:47 +0100

    have (tokenise), still working on (parse)

--- definitions.rkt
+++ definitions.rkt
@@ -43,6 +43,73 @@
 (define (ct-take n lst)
   (take lst n))
 
+;; LANGUAGE STUFF
+
+;(define (parse grammar text)
+;  (define parse-section (try-parse-rules grammar text)) ;; returns (matching-rule-name substring1 ... substringn) or #f
+;  (if parse-section
+;      (list (first parse-section)
+;            (map (λ)
+;      #f)))) ;; couldn't match, backtrack
+
+; (define test-grammar
+;  '(((lst (list (list #\( a ... #\) _))
+;    ((int #rx"^[0-9]+") (sym #rx"^[-a-zA-Z]+")))
+; ;; should do "(hello-world 1 (f 2) 3)" -> (lst (sym "hello-world") (int "1") (lst (sym "f") (int "2")) (int "3"))
+;
+; general: ((prod1 ... prodn) (term1 ... termn))
+
+;(define (parse grammar text)
+;  (define tokenised (tokenise (second grammar) (open-input-string text)))
+;  (parse-tokens (first grammar) tokenised))
+;
+
+; you can turn greedy into lazy by reversing input and prepending _ ..., but it's probably a bad approach - especially since ... needs special handling
+; (match (reverse '(#\( (sym "hello-world") #\space (int "1") #\space #\( (sym "f") #\space (int "2") #\) #\space (int "3") #\) #\( 1 #\)))
+;    [(or (list _ ... #\) a ... #\()
+;         (list #\( a ... #\)))
+;     a])
+
+(define (parse-tokens rules tokens (current-rule '()))
+  (if (empty? (first rules))
+      #f
+      (let* [(productions (first rules)) (current-rule (if (empty? current-rule) (first productions) current-rule)) (terminals (second rules))]
+        (cond
+          [(symbol? (first current-rule))
+           (define term (member (first current-rule) terminals (λ (x y) (equal? x (first y)))))
+           (if term
+               (if (equal? (first (first tokens)) (first (first term)))
+                   (first tokens) ;; stops here, how can we make it continue? do we need to track a current-rule-set and all-rules?
+                   #f)
+               (if (member (first current-rule) productions (λ (x y) (equal? x (first y))))
+                   (write current-rule) ;; figure out what to do here later
+                   #f))]
+          [(char? (first current-rule))
+           (if (eq? (first tokens) (first current-rule))
+               (parse-tokens rules (rest tokens) (rest current-rule))
+               (parse-tokens (list (rest productions) terminals) tokens))]
+          [else (display "shouldn't happen yet")]))))
+
+
+(parse-tokens '(((#\( int #\))) ;; TESTING
+                  ((int whatevs)))
+                '(#\( (int 1) #\)))
+
+;; tokeniser works - be aware that order of terminals matters!
+(define (tokenise terminals text-port (result '()))
+  (if (eq? (peek-char text-port) eof)
+      result
+      (tokenise terminals text-port (append result (list (next-token terminals text-port))))))
+
+(define (next-token terminals text-port)
+  (if (empty? terminals)
+      (read-char text-port)
+      (let ([attempt (regexp-try-match (second (first terminals)) text-port)])
+        (if attempt
+            (list (first (first terminals)) (first attempt))
+            (next-token (rest terminals) text-port)))))
+
+
 ;; override racket definitions
 (provide (rename-out (ct-display display)
                      (ct-write write)
