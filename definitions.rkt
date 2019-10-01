736df7d86619f56d6f44d5266a8b20195ca2434f
asa <asa@localhost.localdomain>
2019-10-02 00:04:41 +0100

    parser done

--- definitions.rkt
+++ definitions.rkt
@@ -1,5 +1,7 @@
 #lang racket
 
+(require (for-syntax racket/list)) ;; for grammar macro
+
 ;; OUTPUT
 
 (define (ct-display #:out (out (current-output-port)) . args)
@@ -45,57 +47,104 @@
 
 ;; LANGUAGE STUFF
 
-;(define (parse grammar text)
-;  (define parse-section (try-parse-rules grammar text)) ;; returns (matching-rule-name substring1 ... substringn) or #f
-;  (if parse-section
-;      (list (first parse-section)
-;            (map (λ)
-;      #f)))) ;; couldn't match, backtrack
+;; parse is done, next up is apply-lang with arbitrary expanders (we can make a nice (rule-expander ...) macro later, but need to support more complex forms so sublanguages can do macros and stuff).
+;; it may be helpful to know that eval with current namespace works in and for citrus:
+;(define-namespace-anchor a)
+;(define ns |namespace-anchor->namespace a)
+;(eval '(display "hello " |cons 1 '|2 3) ns)
+; -> 'hello (1 2 3)'
+;; also for later, a grammar should be able to have an 'ignore-whitespace flag that makes parse remove all whitespace from the lexed source before continuing, so you don't have to deal with it in your grammar by hand.
 
-; (define test-grammar
-;  '(((lst (list (list #\( a ... #\) _))
-;    ((int #rx"^[0-9]+") (sym #rx"^[-a-zA-Z]+")))
-; ;; should do "(hello-world 1 (f 2) 3)" -> (lst (sym "hello-world") (int "1") (lst (sym "f") (int "2")) (int "3"))
-;
-; general: ((prod1 ... prodn) (term1 ... termn))
+;; parse and supporting functions
+(define (parse grammar text)
+  (define tokens (tokenise (second grammar) (open-input-string text)))
+  (let-values ([(parse-result empty-tokens) (parse-next grammar tokens grammar)])
+    parse-result))
 
-;(define (parse grammar text)
-;  (define tokenised (tokenise (second grammar) (open-input-string text)))
-;  (parse-tokens (first grammar) tokenised))
-;
+(define (parse-next current-rules tokens all-rules (result '()))
+  (cond
+    [(empty? (first current-rules))
+      (values #f #f)]
+    [(empty? tokens) (values result '())] ;; finished parse 
+    [else
+     (let-values ([(attempt-item attempt-tokens) (parse-with-rule (first (first current-rules)) tokens all-rules)])
+       (define-values (backtrack-item backtrack-tokens)
+         (if attempt-item
+             (parse-next all-rules attempt-tokens all-rules (append result (list attempt-item)))
+             (parse-next (list (rest (first current-rules)) (second current-rules)) tokens all-rules result)))
+       (if backtrack-item
+           (values backtrack-item backtrack-tokens)
+           (parse-next (list (rest (first current-rules)) (second current-rules)) tokens all-rules result)))]))
 
-; you can turn greedy into lazy by reversing input and prepending _ ..., but it's probably a bad approach - especially since ... needs special handling
-; (match (reverse '(#\( (sym "hello-world") #\space (int "1") #\space #\( (sym "f") #\space (int "2") #\) #\space (int "3") #\) #\( 1 #\)))
-;    [(or (list _ ... #\) a ... #\()
-;         (list #\( a ... #\)))
-;     a])
+(define (parse-with-rule rule tokens rules (result '()))
+  (if (empty? (second rule))
+      (values ;; pass back lazy parse and remaining tokens
+       (cons (first rule) result)
+       tokens)
+      (cond
+        [(empty? tokens)
+         (if (and (list? (first (second rule))) (equal? (first (first (second rule))) '?))
+             (parse-with-rule (list (first rule) (rest (second rule))) tokens rules result)
+             (values #f #f))]
+        [(symbol? (first (second rule)))
+         (cond
+           [(member (first (second rule)) (second rules) (λ (x y) (equal? x (first y)))) ;; terminal
+            (if (and (list? (first tokens)) (equal? (first (first tokens)) (first (second rule))))
+                (parse-with-rule (list (first rule) (rest (second rule))) (rest tokens) rules (append result (list (first tokens))))
+                (values #f #f))]
+           [(member (first (second rule)) (first rules) (λ (x y) (equal? x (first y)))) ;; production
+            (define-values (item-next tokens-next) (parse-with-rule (first (member (first (second rule)) (first rules) (λ (x y) (equal? x (first y))))) tokens rules))
+            ;(parse-next rules tokens rules))
+            (cond
+              [(not item-next) (values #f #f)]
+              [(equal? (first (second rule)) (first item-next))
+               (parse-with-rule (list (first rule) (rest (second rule))) tokens-next rules (append result (list item-next)))] )]
+;              [else (values #f #f)])]
+           [else (error (format "parse error: unrecognised rule '~a': ~a" (first (second rule)) rule))])]
+        [(char? (first (second rule)))
+         (if (eq? (first (second rule)) (first tokens))
+             (parse-with-rule (list (first rule) (rest (second rule))) (rest tokens) rules (append result (list (first tokens))))
+             (values #f #f))]
+        [(list? (first (second rule))) ;; special rule (* ...) or (or ...)
+         (case (first (first (second rule)))
+           [(*) ;; the strategy here is, given ((* a) b c), to try (a b c) and then if that fails (* a a) b c) 
+            (if (> (- (length (first (second rule))) 1) (length tokens)) ;; -1 to count the '*'
+                (values #f #f)
+                (let-values ([(attempt-item attempt-tokens)
+                              (parse-with-rule (list (first rule) (append (rest (first (second rule))) (rest (second rule)))) tokens rules result)])
+                  (if attempt-item
+                      (values attempt-item attempt-tokens)
+                      (parse-with-rule (list (first rule) (cons (append (first (second rule)) (list (second (first (second rule))))) (rest (second rule)))) tokens rules result))))]
+           [(or) ;; similarly, given ((or a b c) d) we try (a d) then (or b c) d)
+            (if (eq? (length (first (second rule))) 1)
+                (values #f #f) ;; no more options
+                (let-values ([(attempt-item attempt-tokens)
+                              (parse-with-rule (list (first rule) (cons (second (first (second rule))) (rest (second rule)))) tokens rules result)])
+                  (if attempt-item
+                      (values attempt-item attempt-tokens)
+                      (parse-with-rule (list (first rule) (cons (cons 'or (drop (first (second rule)) 2)) (rest (second rule)))) tokens rules result))))]
+           [(?) ;; ((? a b) c) -> (a b c), then (c)
+            (let-values ([(attempt-item attempt-tokens)
+                          (parse-with-rule (list (first rule) (append (rest (first (second rule))) (rest (second rule)))) tokens rules result)])
+              (if attempt-item
+                  (values attempt-item attempt-tokens)
+                  (parse-with-rule (list (first rule) (rest (second rule))) tokens rules result)))]
+           [(quote) ;; literal list, given ((quote (a b)) c) try (a b c) only. for use in things like ((or '(a b) c) d)
+            (parse-with-rule (list (first rule) (append (second (first (second rule))) (rest (second rule)))) tokens rules result)]
+           [else (display (format "no such special grammar form: '~a' (accepts '*', 'or' and '?'): ~a" (first (first (second rule))) (second rule)))])]
+        [else (display "shouldn't happen")])))
 
-(define (parse-tokens rules tokens (current-rule '()))
-  (if (empty? (first rules))
-      #f
-      (let* [(productions (first rules)) (current-rule (if (empty? current-rule) (first productions) current-rule)) (terminals (second rules))]
-        (cond
-          [(symbol? (first current-rule))
-           (define term (member (first current-rule) terminals (λ (x y) (equal? x (first y)))))
-           (if term
-               (if (equal? (first (first tokens)) (first (first term)))
-                   (first tokens) ;; stops here, how can we make it continue? do we need to track a current-rule-set and all-rules?
-                   #f)
-               (if (member (first current-rule) productions (λ (x y) (equal? x (first y))))
-                   (write current-rule) ;; figure out what to do here later
-                   #f))]
-          [(char? (first current-rule))
-           (if (eq? (first tokens) (first current-rule))
-               (parse-tokens rules (rest tokens) (rest current-rule))
-               (parse-tokens (list (rest productions) terminals) tokens))]
-          [else (display "shouldn't happen yet")]))))
+(define (parse-with-rule-* rule tokens rules (result '()))
+  (define-values (attempt-item attempt-tokens)
+    (parse-with-rule rule tokens rules result))
+  (if attempt-item
+      (let-values ([(cont-item cont-tokens) (parse-with-rule rule attempt-tokens rules (append result (list attempt-item)))])
+        (if cont-item
+            (values cont-item cont-tokens)
+            (parse-with-rule-* rule attempt-tokens rules (append result (list attempt-item)))))
+      (values #f #f)))
 
-
-(parse-tokens '(((#\( int #\))) ;; TESTING
-                  ((int whatevs)))
-                '(#\( (int 1) #\)))
-
-;; tokeniser works - be aware that order of terminals matters!
+;; tokeniser - be aware that order of terminals matters!
 (define (tokenise terminals text-port (result '()))
   (if (eq? (peek-char text-port) eof)
       result
@@ -108,6 +157,30 @@
         (if attempt
             (list (first (first terminals)) (first attempt))
             (next-token (rest terminals) text-port)))))
+
+;; grammar generating macro
+(define-syntax (grammar stx)
+  (define dtm (syntax->datum stx))
+  (datum->syntax
+   stx
+   `(quote
+     ,(foldl
+       (λ (rule g) (case (first rule)
+                     [(production) (list (cons `(,(second rule) ,(drop rule 2)) (first g)) (second g))] ;; (production name part1 ... partn) -> (name (part1 ... partn)
+                     [(terminal) (list (first g) (cons `(,(second rule) ,(regexp (string-append "^" (third rule)))) (second g)))] ;; (terminal name "regex-string") -> (name #rx"^regex-string")
+                     [else (error (format "unrecognised rule type '~a' (must be terminal or production)" (first rule)))]))
+       '(()())
+       (rest dtm)))))
+
+
+; TEST WORKS:
+;(define test-grammar
+;  (grammar
+;   (production lst (? #\space) #\( (? #\space) (* '(elem (? #\space))) #\) (? #\space))
+;   (production elem (or int sym lst))
+;   (terminal int "[0-9]+")
+;   (terminal sym "[-a-zA-Z]+")))
+;(parse test-grammar "(hello-world 1 (f 2) 3)")
 
 
 ;; override racket definitions
